76807cd14332fc4f2fe2a0d6677046503a2077c0
Abdhilahi Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-02-22 20:53:09 -0700

    Fix Errors

--- shell/setup-apt.bash
+++ shell/setup-apt.bash
@@ -1,46 +1,53 @@
 #!/bin/bash
 
 # APT Virtual Machine/Virtual Private Server Setup
-sudo apt update \ 
-sudo apt -y full-upgrade \ 
-sudo apt -y install apt-transport-https ca-certificates curl git zsh ufw iptables firewalld nftables \ 
-sudo echo 'export PATH="$PATH:/usr/bin"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/bin"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/bin"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/include"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/include"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/include"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/include"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/lib"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/lib"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/lib"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/lib"' >> /etc/skel/.bashrc \ 
-sudo echo '. "$HOME/.cargo/env"' >> ~/.bashrc \ 
-sudo echo '. "$HOME/.cargo/env"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> /etc/skel/.bashrc \ 
-sudo apt -y install gcc gdb llvm clang lldb make cmake ninja \ 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh \ 
-wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz \ tar --extract --file ./*.xz --verbose \ 
-mv ./zig-linux-x86_64-0.13.0 ./zig \ sudo mv ./zig /usr/bin \ 
-wget https://github.com/zigtools/zls/releases/download/0.13.0/zls-x86_64-linux.tar.xz \ tar --extract --file ./*.xz --verbose \ 
-sudo mv ./zls /usr/bin/zig \ 
-sudo add-apt-repository ppa:maveonair/helix-editor \ sudo apt update \ 
-sudo apt -y install ed sed nano vim neovim helix podman \ 
+sudo apt update
+sudo apt -y full-upgrade
+# Install Utilities
+sudo apt -y install apt-transport-https ca-certificates curl git zsh ufw iptables firewalld nftables
+# Export PATH Environment Variables to .bashrc
+sudo echo 'export PATH="$PATH:/usr/bin"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/bin"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/bin"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/include"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/include"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/include"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/include"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/lib"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/lib"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/lib"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/lib"' >> /etc/skel/.bashrc
+# Install Tools
+sudo echo '. "$HOME/.cargo/env"' >> ~/.bashrc
+sudo echo '. "$HOME/.cargo/env"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> /etc/skel/.bashrc
+sudo apt -y install gcc gdb llvm clang lldb make cmake ninja
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz  tar --extract --file ./*.xz --verbose
+mv ./zig-linux-x86_64-0.13.0 ./zig  sudo mv ./zig /usr/bin
+wget https://github.com/zigtools/zls/releases/download/0.13.0/zls-x86_64-linux.tar.xz  tar --extract --file ./*.xz --verbose
+sudo mv ./zls /usr/bin/zig
+sudo add-apt-repository ppa:maveonair/helix-editor  sudo apt update
+sudo apt -y install ed sed nano vim neovim helix podman
 # Add Docker's official GPG key:
-sudo apt update \ sudo install -m 0755 -d /etc/apt/keyrings \ 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc \ sudo chmod a+r /etc/apt/keyrings/docker.asc \ 
+sudo apt -y update
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \ 
-sudo apt update \ sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \ 
-curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube_latest_amd64.deb \ 
-sudo apt -y install minikube_latest_amd64.deb \ 
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.35.0/kompose-linux-amd64 -o kompose \ 
-curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64 \ 
-sudo mv ./kompose ./kops /usr/local/bin \ 
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt -y update
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.35.0/kompose-linux-amd64 -o kompose
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
 sudo install skaffold /usr/local/bin/
--- shell/setup-dnf.bash
+++ shell/setup-dnf.bash
@@ -1,33 +1,37 @@
 #!/bin/bash
 
 # DNF Virtual Machine/Virtual Private Server Setup
-sudo dnf -y upgrade \ 
-sudo dnf -y install dnf-plugins-core curl git zsh ufw iptables firewalld nftables \ 
-sudo echo 'export PATH="$PATH:/usr/bin"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/bin"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/bin"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/include"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/include"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/include"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/include"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/lib"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/lib"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/lib"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/local/lib"' >> /etc/skel/.bashrc \ 
-sudo echo '. "$HOME/.cargo/env"' >> ~/.bashrc \ 
-sudo echo '. "$HOME/.cargo/env"' >> /etc/skel/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> ~/.bashrc \ 
-sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> /etc/skel/.bashrc \ 
-sudo dnf -y install zig gcc gdb llvm clang lldb make cmake ninja \ 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh \ 
-sudo dnf -y install ed sed nano vim neovim helix podman \ 
-sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \ 
-sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \ 
-curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-latest.x86_64.rpm \ 
-sudo dnf -y install minikube-latest.x86_64.rpm \ 
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.35.0/kompose-linux-amd64 -o kompose \ 
-curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64 \ 
-chmod +x ./kompose ./kops \ sudo mv ./kompose ./kops /usr/local/bin \ 
+sudo dnf -y upgrade
+# Install Utilities
+sudo dnf -y install dnf-plugins-core curl git zsh ufw iptables firewalld nftables
+# Export PATH Environment Variables to .bashrc
+sudo echo 'export PATH="$PATH:/usr/bin"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/bin"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/bin"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/include"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/include"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/include"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/include"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/lib"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/lib"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/lib"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/local/lib"' >> /etc/skel/.bashrc
+# Install Tools
+sudo echo '. "$HOME/.cargo/env"' >> ~/.bashrc
+sudo echo '. "$HOME/.cargo/env"' >> /etc/skel/.bashrc
+sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/usr/bin/zig"' >> /etc/skel/.bashrc
+sudo dnf -y install zig gcc gdb llvm clang lldb make cmake ninja
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo dnf -y install ed sed nano vim neovim helix podman
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.35.0/kompose-linux-amd64 -o kompose
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
 sudo install skaffold /usr/local/bin/
