a87d2d6b13ba854d2a7a07d8b8813423e08a6a21
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-03-24 17:47:33 -0600

    Restructure UpD

--- upd/src/arguments/argument_tokenizer.rs
+++ upd/src/arguments/argument_tokenizer.rs
@@ -1,27 +1,25 @@
 use std::env::args;
+use std::process::exit;
 
-use crate::utility::{
-    exit_program::error_exit, 
-    print_help::print_help_message,
-    print_version::print_version_number
-};
+use crate::utility::{print_help::print_help_message, print_version::print_version_number};
 
 use crate::upgrades::{
-    apt_upgrade::upgrade_apt,
-    dnf_release::release_dnf,
-    dnf_upgrade::upgrade_dnf,
-    snap_refresh::refresh_snap,
-    ubuntu_release::release_ubuntu,
+    apt_upgrade::upgrade_apt, dnf_release::release_dnf, dnf_upgrade::upgrade_dnf,
+    snap_refresh::refresh_snap, ubuntu_release::release_ubuntu,
 };
 
 // Command Line Argument Tokenizer
 pub fn tokenize_arguments() -> () {
     let command_line_arguments: Vec<String> = args().collect();
-    
+
     if command_line_arguments.len() != 2 {
-        println!("Command or Flag Required but not Both: {:#?}", command_line_arguments);
+        println!(
+            "Command or Flag Required but not Both: {:#?}",
+            command_line_arguments
+        );
         print_help_message();
-        error_exit();
+        println!("Error(1) - Exiting System Update Daemon");
+        exit(1);
     } else {
         match command_line_arguments[1].trim() {
             "apt-upgrade" | "--au" => {
@@ -46,9 +44,13 @@ pub fn tokenize_arguments() -> () {
                 print_version_number();
             }
             &_ => {
-                println!("Uknown Command or Flag: {:#?}", command_line_arguments[1].trim());
+                println!(
+                    "Uknown Command or Flag: {:#?}",
+                    command_line_arguments[1].trim()
+                );
                 print_help_message();
-                error_exit()
+                println!("Error(1) - Exiting System Update Daemon");
+                exit(1);
             }
         };
     };
--- upd/src/utility.rs
+++ upd/src/utility.rs
@@ -1,5 +1,3 @@
-pub mod exit_program;
-
 pub mod print_help;
 
 pub mod print_version;
