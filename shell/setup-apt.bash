1321efbdb3b7870be9f975d960fe398ea623b9f4
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-05-09 05:27:05 -0600

    Update *.sh

--- shell/setup-apt.bash
+++ shell/setup-apt.bash
@@ -5,17 +5,19 @@ sudo ubuntu-drivers install
 # APT Virtual Machine/Virtual Private Server Setup
 sudo apt update && sudo apt -y full-upgrade
 # Install Utilities
-sudo apt -y install curl ssh apt-transport-https ca-certificates pkg-config linux-tools-generic fdutils git tree zsh libssl-dev ufw iptables firewalld nftables
+sudo apt -y install curl ssh apt-transport-https ca-certificates pkg-config linux-tools-generic fdutils libssl-dev ufw iptables firewalld nftables
 # Install Tools
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt -y install git-lfs gcc gdb llvm clang lldb make cmake ninja
+sudo apt -y install git-lfs gcc gdb llvm clang lldb make cmake ninja tree git zsh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup target add aarch64-apple-darwin
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-unknown-linux-gnu
 rustup target add aarch64-pc-windows-msvc
 rustup target add x86_64-pc-windows-msvc
-sudo apt -y install ed sed nano kakoune podman
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt -y install ed sed nano kakoune micro vim neovim helix podman openssh
 # Add Docker's official GPG key:
 sudo apt -y update
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -37,3 +39,15 @@ sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update && sudo apt -y install brave-browser
+# Install Kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+# Install Skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
+# Install Snaps
+sudo snap install vivaldi krita kolourpaint gnome-system-monitor gnome-logs 0ad warzone2100
--- shell/setup-dnf.bash
+++ shell/setup-dnf.bash
@@ -5,7 +5,7 @@ sudo dnf -y install akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
 # DNF Virtual Machine/Virtual Private Server Setup
 sudo dnf -y upgrade
 # Install Utilities
-sudo dnf -y install ssh dnf-utils dnf-plugins-core curl git zsh openssl openssl-devel ufw iptables firewalld nftables
+sudo dnf -y install ssh dnf-utils dnf-plugins-core curl openssl openssl-devel ufw iptables firewalld nftables
 # Install Tools
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
 rustup target add aarch64-apple-darwin
@@ -13,10 +13,20 @@ rustup target add x86_64-apple-darwin
 rustup target add aarch64-unknown-linux-gnu
 rustup target add aarch64-pc-windows-msvc
 rustup target add x86_64-pc-windows-msvc
-sudo dnf -y install git-lfs gcc gdb llvm clang lldb make cmake ninja
+sudo dnf -y install git-lfs gcc gdb llvm clang lldb make cmake ninja zsh tree git
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo dnf -y install ed sed nano kakoune podman
+sudo dnf -y install ed sed nano kakoune podman micro vim neovim helix openssh
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo dnf-3 config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo dnf -y install brave-browser
+# Install Kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+# Install Skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
