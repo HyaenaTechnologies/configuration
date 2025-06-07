5a58362d7d70a28323b32510e079efbdc80fa0d4
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-06-07 06:58:15 -0600

    Refactor Shell Scripts

--- shell/setup-apt.bash
+++ shell/setup-apt.bash
@@ -6,18 +6,26 @@ sudo ubuntu-drivers install
 sudo apt update && sudo apt -y full-upgrade
 # Install Utilities
 sudo apt -y install curl ssh apt-transport-https ca-certificates pkg-config linux-tools-generic fdutils libssl-dev ufw iptables firewalld nftables
-# Install Tools
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt -y install autoconf automake gcc gdb llvm clang lldb make cmake ninja-build tree git git-lfs zsh rsync librsync-dev
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt -y install ed sed nano micro neovim helix podman openssh ffmpeg imagemagick
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup target add aarch64-apple-darwin
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-unknown-linux-gnu
 rustup target add aarch64-pc-windows-msvc
 rustup target add x86_64-pc-windows-msvc
-sudo add-apt-repository ppa:maveonair/helix-editor
-sudo apt update
-sudo apt -y install ed sed nano micro neovim helix podman openssh ffmpeg imagemagick
+# Install Go Language
+wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz --verbose && tar --extract --file ./*.gz --verbose
+sudo install ./go /usr/local/bin/
+# Install Zig Language
+wget https://ziglang.org/download/0.14.1/zig-x86_64-linux-0.14.1.tar.xz --verbose && tar --extract --file ./*.xz --verbose
+mv ./zig-x86_64-linux-0.14.1 ./zig
+sudo install ./zig /usr/local/bin/
+# Install Docker
 # Add Docker's official GPG key:
 sudo apt -y update
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -35,10 +43,8 @@ echo \
 sudo apt update
 
 sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# Install Boost and Abseil
-sudo apt -y install libboost-all-dev libboost-doc libabsl-dev
-# Install Camera Library
-sudo apt -y install libcamera-dev libcamera-doc
+# Install Boost, Abseil, Google Test, LibCamera and International Components for Unicode
+sudo apt -y install libicu-dev icu-devtools icu-doc libboost-all-dev libboost-doc libabsl-dev googletest libcamera-dev libcamera-doc
 # Install Vulkan
 sudo apt -y install libvulkan-dev libvulkan-memory-allocator-dev libvulkan-volk-dev libvulkan1 spirv-cross vulkan-memory-allocator-doc vulkan-tools vulkan-utility-libraries-dev vulkan-validationlayers libshaderc-dev glslang-dev glslang-tools
 # Install WebM and WebP Image Format Libraries
@@ -49,22 +55,27 @@ sudo apt -y install libusb-dev libhidapi-dev
 sudo apt -y install libfilament-dev libfilament-tools
 # Install Mesa
 sudo apy -y install mesa-vulkan-drivers mesa-vdpau-drivers mesa-va-drivers mesa-utils-bin mesa-utils mesa-libgallium mesa-common-dev mesa-drm-shim mesa-opencl-icd
-# Install International Components for Unicode
-sudo apt -y install libicu-dev icu-devtools icu-doc
+# Install Kubectl
+curl -LO https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl
+sudo install ./kubectl /usr/local/bin/
+# Install Kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo install ./kops /usr/local/bin/
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo apt -y install minikube_latest_amd64.deb
+rm ./minikube_latest_amd64.deb
+# Install Kompose
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.36.0/kompose-linux-amd64 -o kompose
+sudo install ./kompose /usr/local/bin/ 
+# Install Skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+sudo install ./skaffold /usr/local/bin/
 # Install Brave Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update && sudo apt -y install brave-browser
-# Install Kops
-curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
-chmod +x kops
-sudo mv kops /usr/local/bin/kops
-# Install Minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
-# Install Skaffold
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
-sudo install skaffold /usr/local/bin/
 # Install Snaps
 sudo snap install snapcraft --classic
 sudo snap install snap-store snapd-desktop-integration desktop-security-center firmware-updater prompting-client krita 0ad warzone2100
--- shell/setup-dnf.bash
+++ shell/setup-dnf.bash
@@ -4,45 +4,52 @@
 sudo dnf -y upgrade
 # Install Utilities
 sudo dnf -y install ssh dnf-utils dnf-plugins-core curl openssl openssl-devel ufw iptables firewalld nftables
-# Install Tools
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+sudo dnf -y install autoconf automake gcc gdb llvm clang lldb make cmake ninja zig zsh tree git git-lfs rsync librsync
+sudo dnf -y install ed sed nano podman micro neovim helix openssh ffmpeg imagemagick
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup target add aarch64-apple-darwin
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-unknown-linux-gnu
 rustup target add aarch64-pc-windows-msvc
 rustup target add x86_64-pc-windows-msvc
-sudo dnf -y install autoconf automake gcc gdb llvm clang lldb make cmake ninja zsh tree git git-lfs rsync librsync
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo dnf -y install ed sed nano podman micro neovim helix openssh ffmpeg imagemagick
+# Install Go Language
+wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz --verbose && tar --extract --file ./*.gz --verbose
+sudo install ./go /usr/local/bin/
+# Install Docker
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin cri-o cri-tools1.33
-# Install Boost and Abseil
-sudo dnf -y install boost abseil-cpp
-# Install Camera Library
-sudo dnf -y install libcamera
+# Install Boost, Abseil, LibCamera and International Components for Unicode
+sudo dnf -y install boost abseil-cpp icu libcamera
 # Install Vulkan
 sudo dnf -y install vulkan-headers vulkan-loader vulkan-tools spirv-tools spirv-headers-devel
 # Install WebM and WebP Image Format Libraries
-sudo dnf -y install libwebplibwebp-tools 
+sudo dnf -y install libwebp libwebp-tools 
 # Install LibUSB
 sudo dnf -y install libusb-dev libhidapi-dev
 # Install Mesa
 sudo dnf -y install mesa-dri-drivers mesa-filesystem mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers
-# Install International Components for Unicode
-sudo dnf -y install icu
-# Install Brave Browser
-sudo dnf-3 config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo dnf -y install brave-browser
+# Install Kubectl
+curl -LO https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl
+sudo install ./kubectl /usr/local/bin/
 # Install Kops
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
-sudo mv kops /usr/local/bin/kops
+sudo install ./kops /usr/local/bin/
 # Install Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
 sudo rpm -Uvh minikube-latest.x86_64.rpm
+rm ./minikube-latest.x86_64.rpm
+# Install Kompose
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.36.0/kompose-linux-amd64 -o kompose
+sudo install ./kompose /usr/local/bin/
 # Install Skaffold
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
-sudo install skaffold /usr/local/bin/
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+sudo install ./skaffold /usr/local/bin/
+# Install Brave Browser
+sudo dnf-3 config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf -y install brave-browser
 # Install Snapd
 sudo dnf install snapd
 sudo ln -s /var/lib/snapd/snap /snap
