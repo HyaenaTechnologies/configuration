35e61c4b77014b9010b239d3c39e945e1c256abd
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-06-26 20:10:26 -0600

    Fix Typo

--- shell/git-pull-repositories.bash
+++ shell/git-pull-repositories.bash
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 # Run Git Pull Origin in All Git Repositories in the Current Directory and Print the Output
-find ./. -name .git -execdir git pull origin \; | echo
+find ./ -name .git -execdir git pull origin \; | echo
 
