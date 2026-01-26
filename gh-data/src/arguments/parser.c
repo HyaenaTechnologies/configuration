bae7c811629f6625e48ff3d70c3197b70ad277fd
John Matthias Wabwire <124460109+JohnMatthiasWabwire@users.noreply.github.com>
2026-01-25 23:04:33 -0700

    Continue Refactor

--- gh-data/src/arguments/parser.c
+++ gh-data/src/arguments/parser.c
@@ -1,6 +1,54 @@
 #include "./parser.h"
+#include "help_message.h"
+#include "version_message.h"
 
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 // Parse Command Line Arguments
-int8_t parse_arguments(int length, char *arguments[]) { return 0; }
+int8_t parse_arguments(int length, char *arguments[]) {
+  int8_t index;
+  
+  if (length < 2 || length > 2) {
+    printf("\x1b[31;1;3;4mFlag and User Required:\x1b[0m\n");
+    for (index = 0; index < length; index = index + 1) {
+      printf("%s\n", arguments[index]);
+    }
+    printf("");
+    printf("\x1b[31;1;3;4mError(1) - Exiting GitHub Data Tool\x1b[0m");
+  } else if (strcmp(arguments[1], "--following")) {
+    
+  } else if (strcmp(arguments[1], "--f")) {
+    
+  } else if (strcmp(arguments[1], "--repositories")) {
+    
+  } else if (strcmp(arguments[1], "--r")) {
+    
+  } else if (strcmp(arguments[1], "--starred")) {
+    
+  } else if (strcmp(arguments[1], "--s")) {
+    
+  } else if (strcmp(arguments[1], "version")) {
+    print_version();
+  } else if (strcmp(arguments[1], "--v")) {
+    print_version();
+  } else if (strcmp(arguments[1], "help")) {    print_help();
+  } else if (strcmp(arguments[1], "--h")) {
+    print_help();
+  } else if (strcmp(arguments[1], "version")) {
+    print_version();
+  } else if (strcmp(arguments[1], "--v")) {
+    print_version();
+  } else {
+    printf("\x1b[31;1;3;4mUknown Command or Flag:\x1b[0m\n"); 
+    for (index = 0; index < length; index = index + 1) {
+      printf("%s\n", arguments[index]);
+    }
+    printf("");
+    printf("\x1b[31;1;3;4mError(1) - Exiting GitHub Data Tool\x1b[0m");
+  }
+  
+  return 0;
+}
+
--- null-log/src/arguments/parser.c
+++ null-log/src/arguments/parser.c
@@ -1,10 +1,33 @@
 #include "./parser.h"
+#include "help_message.h"
+#include "version_message.h"
 
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 // Parse Command Line Arguments
 int8_t parse_arguments(int length, char *arguments[]) {
-
+  int8_t index;
+  
+  if (length < 1 || length > 1) {
+    printf("\x1b[31;1;3;4mCommand or Flag Required but not Both:\x1b[0m\n");
+    for (index = 0; index < length; index = index + 1) {
+      printf("%s\n", arguments[index]);
+    }
+    printf("\x1b[31;1;3;4mError(1) - Exiting Null Log\x1b[0m");
+  } else if (strcmp(arguments[1], "help")) {
+    print_help();
+  } else if (strcmp(arguments[1], "--h")) {
+    print_help();
+  } else if (strcmp(arguments[1], "version")) {
+    print_version();
+  } else if (strcmp(arguments[1], "--v")) {
+    print_version();
+  } else {
+    
+  }
+  
   return 0;
 }
 
